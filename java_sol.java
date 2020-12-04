99a232d69e8697c01406bf5da5ce845c68d0d90a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-04 10:48:10 +0800

    Create java_sol.java

---
+++ java_sol.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final Reader in = new Reader();
+	static final PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+//		int t=in.nextInt();
+		int t=1;
+		for(int i=1; i<=t; ++i)
+			new Solver();
+		out.close();
+	}
+	
+	static class Solver {
+		Solver() {
+			int n=in.nextInt(), sum=0;
+			for(int i=0; i<n; ++i) {
+				int a=in.nextInt();
+				sum+=Math.abs(a);
+			}
+			out.println(sum);
+		}
+	}
+	
+	static class Reader {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		String next() {
+			while(st==null||!st.hasMoreTokens()) {
+				try {
+					st=new StringTokenizer(in.readLine());
+				} catch(Exception e) {}
+			}	
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+}
